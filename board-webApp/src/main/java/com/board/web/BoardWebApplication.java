8add7523854a4f77055af9e58facb21c610357ee
Ralph HONG <dev.inpyo@gmail.com>
2022-12-21 20:41:47 +0900

    STUDY-0001 : 기초설정

---
+++ board-webApp/src/main/java/com/board/web/BoardWebApplication.java
@@ -0,0 +1,22 @@
+package com.board.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class BoardWebApplication {
+
+    public static final String TIME_ZONE = "Asia/Seoul";
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone(TIME_ZONE));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardWebApplication.class);
+    }
+}
